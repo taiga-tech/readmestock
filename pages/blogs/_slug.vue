eb0eccfa2244ff45a678a2d66ec9c67fcafa7ea2
taiga-tech <taiga.dev.n@gmail.com>
2021-01-17 17:55:40 +0900

    add blogs

---
+++ pages/blogs/_slug.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container>
+    <article class="blog">
+      <div class="text-h4">{{ blogs.title }}</div>
+      <p>{{ blogs.discription }}</p>
+      <nuxt-content :document="blogs" />
+    </article>
+  </v-container>
+</template>
+
+<style lang="scss">
+.blog {
+  box-sizing: border-box;
+  min-width: 200px;
+  max-width: 980px;
+  margin: 0 auto;
+  padding: 45px;
+
+  img {
+    background: none;
+    max-width: 100%;
+  }
+}
+</style>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const blogs = await $content('blogs', params.slug || 'index').fetch()
+    return { blogs }
+  },
+}
+</script>
---
+++ pages/blogs/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container>
+    <v-row justify="start" dense>
+      <v-col
+        v-for="b in blogs"
+        :key="b.slug"
+        cols="12"
+        sm="6"
+        md="4"
+        lg="4"
+        xl="3"
+      >
+        <nuxt-link :to="'/blogs/' + b.slug">
+          <v-card elevation="5" outlined min-height="200" class="pa-4">
+            <div class="text-h5">{{ b.title }}</div>
+            <p>{{ b.discription }}</p>
+          </v-card>
+        </nuxt-link>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const query = await $content('blogs' || 'index').limit(10)
+    const blogs = await query.fetch()
+    return { blogs }
+  },
+}
+</script>
