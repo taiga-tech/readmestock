8fbab583bcf12618b4c4b142406ab14ffca7ecc7
taiga-tech <taiga.dev.n@gmail.com>
2021-01-17 17:55:28 +0900

    add readmes

---
+++ pages/readmes/_slug.vue
@@ -0,0 +1,20 @@
+<template>
+  <readme />
+</template>
+
+<script>
+export default {
+  components: {
+    Readme: () => import('~/components/Readme'),
+  },
+  async asyncData({ params }) {
+    const slug = await params.slug // "/abc" パスにアクセスすると、slug は "abc" になります。
+    return { readmesSlug: slug }
+  },
+  data() {
+    return {
+      readmesSlug: '',
+    }
+  },
+}
+</script>
