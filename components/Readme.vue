65046d185e38f2eb9c69fb3706ea7df5e479e0b2
taiga-tech <taiga.dev.n@gmail.com>
2021-01-23 04:07:07 +0900

    error handling

--- components/Readme.vue
+++ components/Readme.vue
@@ -1,9 +1,8 @@
 <template>
-  <div
-    outlined
-    class="markdown-body line-numbers"
-    v-html="$md.render(md)"
-  ></div>
+  <div>
+    <div v-if="md" class="markdown-body line-numbers" v-html="$md.render(md)" />
+    <error-alert :error="error" :slug="slug" />
+  </div>
 </template>
 
 <style lang="scss">
@@ -22,12 +21,12 @@
 
 @media (max-width: 767px) {
   .markdown-body {
-    padding: 15px;
+    padding: 5px;
   }
 }
 
-code[class*='language-'],
-pre[class*='language-'] {
+pre[class*='language-'],
+code[class*='language-'] {
   text-shadow: none;
   // background-color: rgba(45, 45, 45, 1);
   code[class*='language-'] {
@@ -45,10 +44,14 @@ pre[class*='language-'] {
 import Prism from '~/plugins/prism'
 
 export default {
+  components: {
+    ErrorAlert: () => import('./ErrorAlert'),
+  },
   data() {
     return {
       cors: 'https://cors-anywhere.herokuapp.com/',
       md: '',
+      error: null,
     }
   },
 
@@ -59,9 +62,13 @@ export default {
 
   methods: {
     async getReadme() {
-      const response = await this.$axios.$get(
-        `${this.cors}https://raw.githubusercontent.com/taiga-tech/${this.$parent.readmesSlug}/master/README.md`
-      )
+      const response = await this.$axios
+        .$get(
+          `https://raw.githubusercontent.com/taiga-tech/${this.$parent.slug}/master/README.md`
+        )
+        .catch((error) => {
+          this.error = error
+        })
       this.md = response
     },
   },
