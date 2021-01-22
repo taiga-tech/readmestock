7bb160be61847a448967da3bf1b41e2ae4282a5b
taiga-tech <taiga.dev.n@gmail.com>
2021-01-23 04:32:36 +0900

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 export default {
   // Target (https://go.nuxtjs.dev/config-target)
-  // target: 'static',
+  target: 'static',
   ssr: false,
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
