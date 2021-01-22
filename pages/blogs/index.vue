a7a0c068ae924b169094b30c0aecd68dac9d0f3e
taiga-tech <taiga.dev.n@gmail.com>
2021-01-23 04:10:05 +0900

    Update index.vue

--- pages/blogs/index.vue
+++ pages/blogs/index.vue
@@ -1,32 +1,16 @@
 <template>
-  <v-container>
-    <v-row justify="start" dense>
-      <v-col
-        v-for="b in blogs"
-        :key="b.slug"
-        cols="12"
-        sm="6"
-        md="4"
-        lg="4"
-        xl="3"
-      >
-        <nuxt-link :to="'/blogs/' + b.slug">
-          <v-card elevation="5" outlined min-height="200" class="pa-4">
-            <div class="text-h5">{{ b.title }}</div>
-            <p>{{ b.discription }}</p>
-          </v-card>
-        </nuxt-link>
-      </v-col>
-    </v-row>
-  </v-container>
+  <app-index :results="blogs" param="blogs" />
 </template>
 
 <script>
 export default {
   async asyncData({ $content, params }) {
-    const query = await $content('blogs' || 'index').limit(10)
+    const query = await $content('blogs' || 'index')
     const blogs = await query.fetch()
-    return { blogs }
+
+    return {
+      blogs,
+    }
   },
 }
 </script>
