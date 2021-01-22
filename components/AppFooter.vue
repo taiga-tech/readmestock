9f051d06dbb40a86b15e753523cdd8968c0b9ef8
taiga-tech <taiga.dev.n@gmail.com>
2021-01-23 04:07:26 +0900

    add confbtn

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,6 +1,26 @@
 <template>
   <v-footer app>
+    <v-menu offset-y top :close-on-content-click="false">
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn icon v-bind="attrs" x-small v-on="on">
+          <v-icon>mdi-cog</v-icon>
+        </v-btn>
+      </template>
+      <v-list>
+        <theme-switch />
+      </v-list>
+    </v-menu>
+
     <v-spacer />
+
     <span>&copy; {{ new Date().getFullYear() }} Taiga</span>
   </v-footer>
 </template>
+
+<script>
+export default {
+  components: {
+    ThemeSwitch: () => import('./ThemeSwitch'),
+  },
+}
+</script>
