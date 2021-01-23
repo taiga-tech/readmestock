335eb9ab7c9c9d34dd5dfad0bb3b9b486153c472
taiga-tech <taiga.dev.n@gmail.com>
2021-01-24 08:22:50 +0900

    Update ErrorAlert.vue

--- components/ErrorAlert.vue
+++ components/ErrorAlert.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-alert v-if="error" border="left" outlined type="error">
+  <v-alert border="left" outlined type="error">
     <v-row align="center">
       <v-col class="grow">
         {{ error }}
@@ -8,7 +8,7 @@
         <v-btn
           icon
           small
-          :href="'https://github.com/taiga-tech/' + slug"
+          :href="'https://github.com/taiga-tech/' + $route.params.slug"
           target="_blank"
           ><v-icon>mdi-github</v-icon></v-btn
         >
@@ -24,12 +24,9 @@
 export default {
   props: {
     error: {
-      type: Object,
+      type: Error,
+      required: false,
       default: null,
-    },
-    slug: {
-      type: String,
-      default: '',
     },
   },
 }
