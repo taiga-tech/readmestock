793e52248f285aa348b74475b9031095563d49e1
taiga-tech <taiga.dev.n@gmail.com>
2021-01-24 08:24:56 +0900

    Update index.vue

--- pages/blogs/index.vue
+++ pages/blogs/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <app-index :results="blogs" param="blogs" />
+  <blog-index :results="blogs" />
 </template>
 
 <script>
@@ -10,6 +10,11 @@ export default {
 
     return {
       blogs,
+    }
+  },
+  head() {
+    return {
+      title: 'ブログ一覧',
     }
   },
 }
