c9d5dd081df5b61ba9ca74c94bc70849ecc1a58c
taiga-tech <taiga.dev.n@gmail.com>
2021-01-26 17:38:16 +0900

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,49 @@
 </template>
 
 <script>
+import getRepositories from '~/apollo/queries/getRepositories.graphql'
+
 export default {
+  data() {
+    return {
+      selectSort: {
+        field: 'CREATED_AT',
+        direction: 'DESC',
+      },
+      languages: [],
+    }
+  },
+
+  mounted() {
+    // this.calcLang()
+  },
+
+  methods: {
+    calcLang() {
+      const viewer = this.viewer.repositories.nodes
+      const count = []
+      for (let i = 0; i < viewer.length; i++) {
+        const nodes = viewer[i].languages.nodes
+        for (let i = 0; i < nodes.length; i++) {
+          const languages = nodes[i].name
+          count.push((count[languages] || 0) + 1)
+        }
+      }
+      this.languages = count
+    },
+  },
+
+  apollo: {
+    viewer: {
+      query: getRepositories,
+      variables() {
+        return {
+          orderBy: this.selectSort,
+        }
+      },
+    },
+  },
+
   head() {
     return {
       title: 'Welcome',
