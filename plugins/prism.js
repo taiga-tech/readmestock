57ef704c2da02a4d44eceb18f3c89e5d14304530
taiga-tech <taiga.dev.n@gmail.com>
2021-01-31 00:51:00 +0900

    update highlight

--- plugins/prism.js
+++ plugins/prism.js
@@ -1,8 +1,35 @@
 import Prism from 'prismjs'
 
+// Include the theme plugin: (optional)
 import 'prismjs/themes/prism-tomorrow.css'
-import 'prismjs/components/prism-bash'
+
+// Include the toolbar plugin: (optional)
+import 'prismjs/plugins/toolbar/prism-toolbar'
+import 'prismjs/plugins/toolbar/prism-toolbar.css'
+
+// Include the line numbers plugin: (optional)
 import 'prismjs/plugins/line-numbers/prism-line-numbers.min.js'
 import 'prismjs/plugins/line-numbers/prism-line-numbers.css'
+
+// Include the line highlight plugin: (optional)
+import 'prismjs/plugins/line-highlight/prism-line-highlight'
+import 'prismjs/plugins/line-highlight/prism-line-highlight.css'
+
+// Include some other plugins: (optional)
+import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard'
+import 'prismjs/plugins/highlight-keywords/prism-highlight-keywords'
+import 'prismjs/plugins/show-language/prism-show-language'
+
+// Include the languages plugin: (optional)
+import 'prismjs/components/prism-markup-templating'
+import 'prismjs/components/prism-bash'
+import 'prismjs/components/prism-scss'
+import 'prismjs/components/prism-javascript'
+import 'prismjs/components/prism-ruby'
+import 'prismjs/components/prism-php'
+import 'prismjs/components/prism-docker'
+import 'prismjs/components/prism-yaml'
+import 'prismjs/components/prism-json'
+import 'prismjs/components/prism-git'
 
 export default Prism
