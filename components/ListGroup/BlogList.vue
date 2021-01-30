f522589d6aa88e5142c9d7206aa4c2a077a354a9
taiga-tech <taiga.dev.n@gmail.com>
2021-01-31 00:54:36 +0900

    ref drawer

---
+++ components/ListGroup/BlogList.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-list-group>
+    <template v-slot:activator>
+      <v-list-item-icon>
+        <v-icon>mdi-post-outline</v-icon>
+      </v-list-item-icon>
+      <v-list-item-content>
+        <v-list-item-title v-text="'Blog'" />
+      </v-list-item-content>
+    </template>
+
+    <v-list-item to="/blogs" router exact>
+      <v-list-item-icon>
+        <v-icon small>mdi-post</v-icon>
+      </v-list-item-icon>
+      <v-list-item-content>
+        <v-list-item-title v-text="'一覧'" />
+      </v-list-item-content>
+    </v-list-item>
+
+    <v-virtual-scroll :items="blogs" height="300" item-height="48">
+      <template v-slot:default="{ item }">
+        <v-list-item :key="item.path" :to="item.path" router exact>
+          <v-list-item-icon>
+            <v-icon small>mdi-post</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </template>
+    </v-virtual-scroll>
+  </v-list-group>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      blogs: [],
+    }
+  },
+
+  mounted() {
+    this.getData()
+  },
+
+  methods: {
+    async getData() {
+      const query = await this.$content('blogs' || 'index')
+      const blogs = await query.fetch()
+      this.blogs = blogs
+    },
+  },
+}
+</script>
---
+++ components/ListGroup/ReadmeList.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-list-group>
+    <template v-slot:activator>
+      <v-list-item-icon>
+        <v-icon>mdi-github</v-icon>
+      </v-list-item-icon>
+      <v-list-item-content>
+        <v-list-item-title v-text="'README'" />
+      </v-list-item-content>
+    </template>
+
+    <v-list-item to="/readmes" router exact>
+      <v-list-item-icon>
+        <v-icon small>mdi-github</v-icon>
+      </v-list-item-icon>
+      <v-list-item-content>
+        <v-list-item-title v-text="'一覧'" />
+      </v-list-item-content>
+    </v-list-item>
+
+    <v-virtual-scroll
+      :items="viewer.repositories.nodes"
+      height="300"
+      item-height="48"
+    >
+      <template v-slot:default="{ item }">
+        <v-list-item :key="item.url" :to="'/readmes/' + item.name" router exact>
+          <v-list-item-icon>
+            <v-icon small>mdi-github</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.name" />
+          </v-list-item-content>
+        </v-list-item>
+      </template>
+    </v-virtual-scroll>
+  </v-list-group>
+</template>
+
+<script>
+export default {
+  props: {
+    viewer: {
+      type: Object,
+      default: null,
+    },
+  },
+}
+</script>
