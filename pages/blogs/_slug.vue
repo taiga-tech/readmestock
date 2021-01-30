8559fdb418f84a2ce6daa329c77796cdea6267d6
taiga-tech <taiga.dev.n@gmail.com>
2021-01-31 01:06:51 +0900

    Update _slug.vue

--- pages/blogs/_slug.vue
+++ pages/blogs/_slug.vue
@@ -1,33 +1,85 @@
 <template>
-  <article class="blog">
+  <div>
     <v-alert border="left" outlined type="warning" dismissible>
       現在準備中です。準備出来次第投稿していきます！
     </v-alert>
-    <div>
-      <div class="text-h4">{{ blogs.title }}</div>
-      <v-chip-group column>
-        <v-chip
-          v-for="(tag, i) in blogs.tags"
+    <v-card
+      elevation="0"
+      class="markdown-body"
+      max-width="980"
+      color="#00000000"
+    >
+      <v-card-title :class="$vuetify.breakpoint.xs ? 'text-h5' : 'text-h4'">
+        {{ blogs.title }}
+      </v-card-title>
+      <v-card-subtitle>
+        <div align="end">
+          <div>
+            作成日: {{ blogs.createdAt }}
+            <!-- {{ $moment(blogs.createdAt).calendar() }} -->
+          </div>
+          <div>最終更新日: {{ $moment(blogs.updatedAt).fromNow() }}</div>
+        </div>
+        <v-chip-group column>
+          <v-chip
+            v-for="(tag, i) in blogs.tags"
+            :key="i"
+            outlined
+            draggable
+            small
+            label
+            >{{ tag }}</v-chip
+          >
+        </v-chip-group>
+      </v-card-subtitle>
+      <nuxt-content :document="blogs" />
+    </v-card>
+
+    <!-- <v-navigation-drawer
+      fixed
+      floating
+      clipped
+      right
+      app
+      :value="true"
+      color="#00000000"
+      class="py-4 pr-3"
+    >
+      <ul style="list-style: none">
+        <li
+          v-for="(to, i) in blogs.toc"
           :key="i"
-          outlined
-          draggable
-          small
-          label
-          >{{ tag }}</v-chip
+          class="pl-3 text-body-2 py-1 font-weight-regular text--disabled"
+          :class="to.depth == 2 ? 'pl-0' : 'pl-6'"
         >
-      </v-chip-group>
-    </div>
-    <v-divider class="ma-3" />
-    <nuxt-content :document="blogs" />
-  </article>
+          {{ to.text }}
+          <v-divider />
+        </li>
+      </ul>
+    </v-navigation-drawer>
+    <pre>{{ blogs.body.children }}</pre> -->
+  </div>
 </template>
 
 <script>
+import Prism from '~/plugins/prism'
+
 export default {
   async asyncData({ $content, params }) {
     const blogs = await $content('blogs', params.slug || 'index').fetch()
     return { blogs }
   },
+
+  data() {
+    return {
+      tree: [],
+    }
+  },
+
+  mounted() {
+    Prism.highlightAll()
+  },
+
   head() {
     return {
       title: this.blogs.title,
@@ -43,16 +95,26 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-.blog {
-  box-sizing: border-box;
-  min-width: 200px;
-  max-width: 980px;
-  margin: 0 auto;
-
-  img {
-    background: none;
-    max-width: 100%;
+<style lang="scss">
+.markdown-body {
+  pre[class*='language-'],
+  code[class*='language-'] {
+    text-shadow: none;
+    // background-color: rgba(45, 45, 45, 1);
+    code[class*='language-'] {
+      background-color: rgba(45, 45, 45, 1);
+    }
+    span.token.operator {
+      background: rgba(45, 45, 45, 0);
+      background-color: rgba(45, 45, 45, 0);
+    }
+    .token.operator,
+    .token.entity,
+    .token.url,
+    .language-css .token.string,
+    .style .token.string {
+      background: none;
+    }
   }
 }
 </style>
