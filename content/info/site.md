800e68209ed2a98310b5c28c1e7412b0d40b66d9
taiga-tech <taiga.dev.n@gmail.com>
2021-02-02 06:55:50 +0900

    add sitepage

---
+++ content/info/site.md
@@ -0,0 +1,21 @@
+---
+title: このサイトについて
+discription: このサイトについて
+---
+
+# このサイトについて
+
+## 開発環境
+||Version|
+|---|:---|
+|Mac OS|Catalina 10.15.7|
+|Node|v15.4.0|
+|npm|7.0.15|
+|Nuxt|2.14.12|
+|Vue|2.6.12|
+|Vuetify|2.4.2|
+
+## 参考サイト
+
+- https://docs.github.com/en/graphql/overview/explorer
+- https://vuetifyjs.com/ja/
---
+++ pages/info/_slug.vue
@@ -0,0 +1,53 @@
+<template>
+  <article>
+    <warning-alert style="max-width: 940px; margin: 0 auto" />
+    <v-card
+      elevation="0"
+      class="markdown-body"
+      max-width="980"
+      color="#00000000"
+    >
+      <nuxt-content :document="info" />
+      <v-card-subtitle align="end">
+        {{ $moment(info.createdAt).format('L') }} -
+        {{ $moment(info.updatedAt).fromNow() }}
+      </v-card-subtitle>
+    </v-card>
+  </article>
+</template>
+
+<script>
+import Prism from '~/plugins/prism'
+
+export default {
+  components: {
+    WarningAlert: () => import('~/components/WarningAlert'),
+  },
+  async asyncData({ $content, params, payload }) {
+    if (payload) {
+      return { info: payload }
+    } else {
+      return { info: await $content('info', params.slug || 'index').fetch() }
+    }
+  },
+
+  mounted() {
+    Prism.highlightAll()
+  },
+
+  head() {
+    return {
+      title: this.info.title,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.info.discription,
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style lang="scss" src="~/assets/scss/custom.scss"></style>
