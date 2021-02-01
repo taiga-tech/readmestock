8ef21982e7caf7a7e3568cc7a7bc19b831d3eac0
taiga-tech <taiga.dev.n@gmail.com>
2021-02-01 19:56:13 +0900

    Create ContactSucsess.vue

---
+++ components/Contacts/ContactSucsess.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-card max-width="940" class="mx-auto py-auto" color="inherit">
+    <v-card-title>お問い合わせありがとうございました</v-card-title>
+    <v-card-text>
+      <div></div>
+      <v-btn text to="/">戻る</v-btn>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    response: {
+      type: Object,
+      default: null,
+    },
+    // error: {
+    //   type: Error,
+    //   default: null,
+    // },
+  },
+}
+</script>
