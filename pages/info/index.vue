4a2c48208cf4cf1afb65e6e1285cd9c422446b56
taiga-tech <taiga.dev.n@gmail.com>
2021-02-05 09:01:01 +0900

    クローリング対策

---
+++ pages/info/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <v-list>
+      <v-list-item v-for="item in info" :key="item.name" :to="item.path + '/'">
+        <v-list-item-content>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, payload }) {
+    if (payload) {
+      return { info: payload }
+    } else {
+      return { info: await $content('info' || 'index').fetch() }
+    }
+  },
+}
+</script>
