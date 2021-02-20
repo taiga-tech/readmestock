fb4bc34065310c44f2e2589e309f51fe7390bdc8
taiga-tech <taiga.dev.n@gmail.com>
2021-02-21 05:20:18 +0900

    add container

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,38 +1,40 @@
 <template>
   <v-app>
-    <v-alert
-      v-if="error.statusCode === 404 || error.statusCode === 500"
-      border="left"
-      type="error"
-      outlined
-    >
-      <v-row align="center">
-        <v-col class="grow">
-          <strong>{{ error.statusCode }} : {{ error.message }}</strong>
-        </v-col>
-        <v-col class="shrink">
-          <v-tooltip bottom>
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn
-                icon
-                href="https://github.com/taiga-tech/"
-                target="_blank"
-                v-bind="attrs"
-                v-on="on"
-                ><v-icon>mdi-github</v-icon></v-btn
-              >
-            </template>
-            <span>Go to Github</span>
-          </v-tooltip>
-        </v-col>
-        <v-col class="shrink">
-          <v-btn text @click="$router.go(-1)">back</v-btn>
-        </v-col>
-      </v-row>
-    </v-alert>
-    <v-alert v-else border="left" outlined type="error">
-      <strong>{{ otherError }}</strong>
-    </v-alert>
+    <v-container>
+      <v-alert
+        v-if="error.statusCode === 404 || error.statusCode === 500"
+        border="left"
+        type="error"
+        outlined
+      >
+        <v-row align="center">
+          <v-col class="grow">
+            <strong>{{ error.statusCode }} : {{ error.message }}</strong>
+          </v-col>
+          <v-col class="shrink">
+            <v-tooltip bottom>
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                  icon
+                  href="https://github.com/taiga-tech/"
+                  target="_blank"
+                  v-bind="attrs"
+                  v-on="on"
+                  ><v-icon>mdi-github</v-icon></v-btn
+                >
+              </template>
+              <span>Go to Github</span>
+            </v-tooltip>
+          </v-col>
+          <v-col class="shrink">
+            <v-btn text @click="$router.go(-1)">back</v-btn>
+          </v-col>
+        </v-row>
+      </v-alert>
+      <v-alert v-else border="left" outlined type="error">
+        <strong>{{ otherError }}</strong>
+      </v-alert>
+    </v-container>
   </v-app>
 </template>
 
