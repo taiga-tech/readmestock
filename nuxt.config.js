9b0cb31f7be1b3788b682ea29431dcebf5381ae4
taiga-tech <taiga.dev.n@gmail.com>
2021-02-28 06:13:11 +0900

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -154,5 +154,11 @@ export default {
   },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {},
+  build: {
+    extend(config, { isClient }) {
+      if (isClient) {
+        config.devtool = 'source-map'
+      }
+    },
+  },
 }
