d88338e597c5d567f491be1fe069677bc59aa183
taiga-tech <taiga.dev.n@gmail.com>
2021-03-09 23:20:42 +0900

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -67,7 +67,7 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
-    // '~/plugins/prism.js',
+    '~/plugins/prism.js',
     // '~/plugins/vue-chart.js',
   ],
 
