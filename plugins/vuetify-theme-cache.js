af23f11c18199a4a77f54f06f9a8de279cad6387
taiga-tech <taiga.dev.n@gmail.com>
2021-04-27 16:03:59 +0900

    Create vuetify-theme-cache.js

---
+++ plugins/vuetify-theme-cache.js
@@ -0,0 +1,10 @@
+const LRU = require('lru-cache')
+
+const themeCache = new LRU({
+  max: 10,
+  maxAge: 1000 * 60 * 60, // 1 hour
+})
+
+export default (ctx) => {
+  ctx.$vuetify.theme.options.themeCache = themeCache
+}
