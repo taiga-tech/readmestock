c36e8989d43a111accf502f3d1e889bb30d05069
taiga-tech <taiga.dev.n@gmail.com>
2021-04-27 16:04:07 +0900

    Create AppSns.vue

---
+++ components/AppSns.vue
@@ -0,0 +1,25 @@
+<template>
+  <span>
+    <v-btn
+      v-for="(sns, i) in socials"
+      :key="i"
+      icon
+      :color="sns.color"
+      :href="sns.href"
+      target="_blank"
+      rel="noopener"
+    >
+      <v-icon medium>mdi-{{ sns.icon }}</v-icon>
+    </v-btn>
+  </span>
+</template>
+
+<script>
+import socials from '~/assets/json/socials.json'
+
+export default {
+  data() {
+    return { socials }
+  },
+}
+</script>
