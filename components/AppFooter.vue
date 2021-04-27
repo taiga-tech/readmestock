f168107a3e10d99518ed9f72ea7b9fce5375befb
taiga-tech <taiga.dev.n@gmail.com>
2021-04-27 16:04:10 +0900

    Update AppFooter.vue

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,16 +1,5 @@
 <template>
   <v-footer height="120" color="#00000000" class="mt-4">
-    <!-- <v-menu offset-y top :close-on-content-click="false">
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn icon v-bind="attrs" x-small v-on="on">
-          <v-icon>mdi-cog</v-icon>
-        </v-btn>
-      </template>
-      <v-list>
-        <theme-switch />
-      </v-list>
-    </v-menu> -->
-
     <v-row justify="center" no-gutters>
       <v-btn
         v-for="link in links"
@@ -24,42 +13,20 @@
 
       <v-col class="py-4 text-center" cols="12">
         &copy; 2021 Taiga
-        <v-btn
-          icon
-          href="https://github.com/taiga-tech/"
-          target="_blank"
-          rel="noopener"
-          aria-label="github"
-          ><v-icon color="purple">mdi-github</v-icon></v-btn
-        >
-        <v-btn
-          icon
-          href="https://twitter.com/Taiga_dev/"
-          target="_blang"
-          rel="noopener"
-          aria-label="twitter"
-          ><v-icon color="primary">mdi-twitter</v-icon></v-btn
-        >
+
+        <app-sns />
       </v-col>
     </v-row>
   </v-footer>
 </template>
 
 <script>
-export default {
-  components: {
-    // ThemeSwitch: () => import('./ThemeSwitch'),
-  },
+import socials from '~/assets/json/socials.json'
+import links from '~/assets/json/links.json'
 
+export default {
   data() {
-    return {
-      links: [
-        { name: 'readme', to: '/readmes/' },
-        { name: 'blog', to: '/blogs/' },
-        { name: 'このサイトについて', to: '/info/about/' },
-        { name: 'お問い合わせ', to: '/info/contact/' },
-      ],
-    }
+    return { links, socials }
   },
 }
 </script>
