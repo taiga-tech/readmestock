b5249f2e72a389e58a02d023ad913d9f1df7040a
taiga-tech <taiga.dev.n@gmail.com>
2021-07-23 05:00:53 +0900

    Create PrevNext.vue

---
+++ components/App/PrevNext.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-card-actions>
+    <v-btn
+      v-if="prev"
+      text
+      :to="prev.path"
+      class="text-decoration-none text-capitalize"
+      color="#55a0f4"
+    >
+      <v-icon color="#fff" left>mdi-arrow-left</v-icon>
+      {{ prev.title }}
+    </v-btn>
+
+    <v-spacer />
+
+    <v-btn
+      v-if="next"
+      text
+      :to="next.path"
+      class="text-decoration-none text-capitalize"
+      color="#55a0f4"
+    >
+      {{ next.title }}
+      <v-icon color="#fff" right>mdi-arrow-right</v-icon>
+    </v-btn>
+    <!-- </div> -->
+  </v-card-actions>
+</template>
+
+<script>
+export default {
+  props: {
+    prev: { type: Object, default: () => null },
+    next: { type: Object, default: () => null },
+  },
+}
+</script>
