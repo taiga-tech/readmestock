95857ffc56aa7cdea593aca68fab324c70bd1f96
taiga-tech <taiga.dev.n@gmail.com>
2021-07-23 05:02:51 +0900

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,49 +1,35 @@
 <template>
   <v-container>
     <v-row>
-      <v-col cols="12" sm="12" md="5" lg="4" xl="4">
-        <lang-chart :user="user" :viewer="viewer" />
-
-        <top-sns :user="user" :viewer="viewer" />
+      <v-col cols="12" sm="12" md="5" lg="4" xl="3">
+        <top-chart :user="user" :viewer="viewer" />
+        <top-sns />
       </v-col>
 
-      <v-col cols="12" sm="12" md="7" lg="8" xl="8">
-        <top-profile />
+      <v-col cols="12" sm="12" md="7" lg="8" xl="9">
+        <top-profile :profile="profile" />
       </v-col>
     </v-row>
 
     <v-row class="mb-0">
       <top-product />
     </v-row>
-
-    <app-footer class="hidden-sm-and-down" />
   </v-container>
 </template>
 
 <script>
-import getPublicTotalCount from '~/apollo/queries/global/getPublicTotalCount.graphql'
-import getUser from '~/apollo/queries/global/getUser.graphql'
-
 export default {
-  components: {
-    LangChart: () => import('~/components/Top/Chart'),
-    TopSns: () => import('~/components/Top/Sns'),
-    TopProduct: () => import('~/components/Top/Product'),
-    TopProfile: () => import('~/components/Top/MyProfile'),
-    AppFooter: () => import('~/components/AppFooter'),
+  async asyncData({ $content }) {
+    const profile = await $content('info', 'profile').fetch()
+    return { profile }
   },
 
-  apollo: {
-    user: {
-      query: getUser,
-      variables() {
-        return {
-          login: 'taiga-tech',
-        }
-      },
+  computed: {
+    user() {
+      return this.$store.getters['gh-readme/user']
     },
-    viewer: {
-      query: getPublicTotalCount,
+    viewer() {
+      return this.$store.getters['gh-readme/count']
     },
   },
 
