0719e95252e59bcf4644ec02a05682d1c8c59feb
taiga-tech <taiga.dev.n@gmail.com>
2021-07-23 02:54:34 +0900

    Rename worning

--- components/App/Warning.vue
+++ components/App/Warning.vue
@@ -1,5 +1,6 @@
 <template>
-  <v-alert border="left" outlined type="warning" dismissible>
+  <v-alert border="left" text type="warning" dismissible>
     現在準備中です。準備出来次第投稿していきます！
+    <slot />
   </v-alert>
 </template>
