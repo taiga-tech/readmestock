803041170dc40a7914023b08b4ed59b5ce3292b6
taiga-tech <taiga.dev.n@gmail.com>
2024-02-07 17:44:44 +0900

    Update domain

--- assets/constants/products.js
+++ assets/constants/products.js
@@ -4,31 +4,36 @@ export const products = [
   {
     name: 'wedding-photo-nuxt',
     icon: mdiNuxt,
+    description: '',
     color: '#04c58e',
     href: 'https://wedding-photo-app.netlify.app/',
   },
   {
     name: 'wedding-photo-api',
     icon: mdiLaravel,
+    description: '',
     color: '#ff291a',
     href: 'https://wedding-photo-app.netlify.app/',
   },
   {
     name: 'DivingApp',
     icon: mdiLaravel,
+    description: '',
     color: '#ff291a',
     'href-comment': 'https://divingapp.ga/',
   },
   {
     name: 'ReservationApp',
     icon: mdiLanguageRubyOnRails,
+    description: '',
     color: '#cc0202',
     'href-comment': 'https://reservationapp.tk/',
   },
   {
     name: 'ReadmeStock',
     icon: mdiNuxt,
+    description: '',
     color: '#04c58e',
-    href: 'https://www.taiga-tech.tk/',
+    href: 'https://taiga-tech.netlify.app/',
   },
 ]
